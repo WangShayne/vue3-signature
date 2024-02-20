01df89906f32ac69169d5f528f4027525169ef5a
phk422 <1769476785@qq.com>
2024-02-20 17:03:55 +0800

    fix: Fixed type errors caused by global registration of components in the typescript environment

--- lib/index.ts
+++ lib/index.ts
@@ -1,11 +1,4 @@
 import Vue3Signature from "./Vue3Signature.vue";
+import { withInstall } from './utils'
 
-import type { App } from "vue";
-
-const install = (app: App) => {
-    app.component("Vue3Signature", Vue3Signature);
-}
-
-Vue3Signature.install = install;
-
-export default Vue3Signature;
+export default withInstall(Vue3Signature);
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+
+import type { App, Plugin, Component } from "vue";
 export interface Watermark {
   text?: string;
   font?: string;
@@ -8,4 +10,12 @@ export interface Watermark {
   fillStyle?: string
   strokeStyle?: string
   style?: "all" | "stroke" | "fill"
+}
+export type SFCWithInstall<T> = T & Plugin
+
+export function withInstall<T>(c: T) {
+    (c as SFCWithInstall<T>).install = (app: App) => {
+        app.component((c as Component).name, c);
+    }
+    return c as SFCWithInstall<T>
 }
